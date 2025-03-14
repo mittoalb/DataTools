a3fbf38ff36a6ede30c26a6abbfa7d48c3ae1ef9
Alberto Mittone <amittone@anl.gov>
2025-03-13 21:53:17 -0500

    Docs base

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,19 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+import sphinx_rtd_theme
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme'
+]
+
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+
+
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +36,4 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
